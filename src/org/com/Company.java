cd295c7e5923c0688a356836a8a050d59f784516
praveencrim <praveencrim1304@gmail.com>
2022-08-28 19:53:03 +0530

    new program

---
+++ src/org/com/Company.java
@@ -0,0 +1,10 @@
+package org.com;
+
+public class Company {
+	public void compId() {
+		System.out.println("4044");
+	}
+	public void comName() {
+		System.out.println("New India");
+	}
+}
---
+++ src/org/emp/Employee.java
@@ -0,0 +1,20 @@
+package org.emp;
+
+import org.com.Company;
+
+public class Employee {
+	private void empId() {
+		System.out.println("0079");
+	}
+	private void empName() {
+		System.out.println("Pream");
+	}
+	public static void main(String[] args) {
+		Company c = new Company();
+		c.compId();
+		c.comName();
+		Employee e = new Employee();
+		e.empId();
+		e.empName();
+	}
+}
